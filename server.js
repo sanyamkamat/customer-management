54d67b750c2c898aeb333d007a606af0e6ce2a21
Sanyam Kamat <sanyamkamat@gmail.com>
2018-05-21 03:08:38 +0530

    remove wrong redirection and change the output pathˆ

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
